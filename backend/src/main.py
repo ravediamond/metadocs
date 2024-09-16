0f9ad09e3c79b3d01b2914339c8410e6f3c8ed24
ravindu.somawansa <ravindu.somawansa@gmail.com>
2024-09-16 11:56:31 +0200

    feat: made offline works for the api

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,11 @@
-# src/main.py
-
 from fastapi import FastAPI
+from mangum import Mangum
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello, this is a dummy function for testing FastAPI with AWS Lambda URLs!"}
+
+
+handler = Mangum(app)
